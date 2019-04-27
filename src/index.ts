31164e64f90e4fd8fbb7697f944da87dda5b97da
xl1 <shiba200712@gmail.com>
2019-04-27 23:54:04 +0900

    Rewrite

--- src/index.ts
+++ src/index.ts
@@ -15,31 +15,154 @@ async function main() {
 
     const { fnArray, argsArray } = await page.getOperatorList();
     const { items } = await page.getTextContent();
+    const lines = extractLines(fnArray, argsArray);
+    const normalized = normalizeLines(lines);
+    const edges = separateToEdges(normalized);
+    const rects = buildRects(edges);
+    const annotatedRects = annotateRects(rects, items);
 
-    const vertices = enumerateVertices(fnArray, argsArray);
-    const labels = enumerateLabels(Array.from(vertices), items);
-
-    for (const label of labels) {
-        console.log(label.name, label.rank, label.top, label.bottom);
+    for (const rect of annotatedRects) {
+        console.log(...rect.lb, ...rect.rt, ...rect.strings);
     }
 }
 
 main().catch(console.error);
 
-function* pairwise(iterable : Iterable<number>): IterableIterator<vec2> {
-    const iter = iterable[Symbol.iterator]();
-    while (true) {
-        const x = iter.next();
-        if (x.done) break;
-        const y = iter.next();
-        if (y.done) break;
-        yield vec2.fromValues(x.value, y.value);
+interface Edge {
+    start: vec2;
+    end: vec2;
+}
+
+interface Rect {
+    lb: vec2;
+    rt: vec2;
+}
+
+interface TextRect extends Rect {
+    strings: string[];
+}
+
+const EPSILON = 0.001;
+const THRESHOLD = 2;
+
+function equals(v: vec2, w: vec2) {
+    return vec2.distance(v, w) < THRESHOLD;
+}
+
+function getCrossingPoint(l1: Edge, l2: Edge): vec2|undefined {
+    const
+        cross = (v: vec2, w: vec2) => v[0] * w[1] - v[1] * w[0],
+        v0 = vec2.subtract(vec2.create(), l2.start, l1.start),
+        v1 = vec2.subtract(vec2.create(), l1.end, l1.start),
+        v2 = vec2.subtract(vec2.create(), l2.end, l2.start),
+        t1 = cross(v2, v0) / cross(v2, v1),
+        t2 = cross(v1, v0) / cross(v2, v1);
+    if ((-EPSILON < t1 && t1 < 1 + EPSILON && 0 < t2 && t2 < 1) ||
+        (-EPSILON < t2 && t2 < 1 + EPSILON && 0 < t1 && t1 < 1)) {
+        return vec2.scaleAndAdd(vec2.create(), l1.start, v1, t1);
+    }
+    return;
+}
+
+function* normalizeLines(lines: Iterable<Edge>): IterableIterator<Edge> {
+    for (const line of lines) {
+        if (Math.abs(line.start[0] - line.end[0]) < THRESHOLD) {
+            // vertical line
+            // order by Y
+            yield line.start[1] < line.end[1]
+                ? line
+                : { start: line.end, end: line.start };
+        } else if (Math.abs(line.start[1] - line.end[1]) < THRESHOLD) {
+            // horizontal line
+            // order by X
+            yield line.start[0] < line.end[0]
+                ? line
+                : { start: line.end, end: line.start };
+        }
     }
 }
 
-function* enumerateVertices(fnArray: OPS[], argsArray: any[]): IterableIterator<vec2> {
+function separateToEdges(lines: Iterable<Edge>): Edge[] {
+    function split(edge: Edge, p: vec2): [Edge, Edge] {
+        return [
+            { start: edge.start, end: p },
+            { start: p, end: edge.end }
+        ];
+    }
+
+    function next(c: Edge, results: Edge[]): [Edge[], Edge[]] {
+        if (equals(c.start, c.end)) {
+            // do not use c
+            return [[], results];
+        }
+        for (let i = 0; i < results.length; i++) {
+            const e = results[i];
+            if (equals(e.start, c.start) && equals(e.end, c.end)) {
+                // do not use c
+                return [[], results];
+            }
+            const p = getCrossingPoint(e, c);
+            if (p) {
+                // remove e from result
+                // add split(e) to result
+                // add split(c) to candidate
+                results.splice(i, 1, ...split(e, p));
+                return [split(c, p), results];
+            }
+        }
+        results.push(c);
+        return [[], results];
+    }
+
+    let candidates: Edge[] = [...lines];
+    let results: Edge[] = [];
+    while (candidates.length) {
+        const newCandidates = [];
+        for (const c of candidates) {
+            const [add, newResults] = next(c, results);
+            newCandidates.push(...add);
+            results = newResults;
+        }
+        candidates = newCandidates;
+    }
+    return results;
+}
+
+function* buildRects(edges: Edge[]): IterableIterator<Rect> {
+    for (const l1 of edges)
+    for (const l2 of edges)
+    for (const l3 of edges)
+    for (const l4 of edges) {
+        if (equals(l1.start, l2.start) &&
+            equals(l1.end, l3.start) &&
+            equals(l2.end, l4.start) &&
+            equals(l3.end, l4.end)) {
+            yield { lb: l1.start, rt: l4.end };
+        }
+    }
+}
+
+function* annotateRects(rects: Iterable<Rect>, texts: pdfjs.TextContentItem[])
+    : IterableIterator<TextRect> {
+    for (const rect of rects) {
+        const strings: string[] = [];
+        for (const text of texts) {
+            const tx = text.transform[4], ty = text.transform[5];
+            if (rect.lb[0] <= tx && tx <= rect.rt[0] &&
+                rect.lb[1] <= ty && ty <= rect.rt[1]) {
+                strings.push(text.str);
+            }
+        }
+        yield { strings, lb: rect.lb, rt: rect.rt };
+    }
+}
+
+function* extractLines(fnArray: OPS[], argsArray: any[]): IterableIterator<Edge> {
     let currentMatrix = mat2d.create();
+    let currentPoint: vec2|undefined;
     const matStack: mat2d[] = [];
+    const vec = (x: number, y: number) =>
+        vec2.transformMat2d(vec2.create(), vec2.fromValues(x, y), currentMatrix);
 
     for (let i = 0; i < fnArray.length; i++) {
         const args = argsArray[i];
@@ -56,71 +179,29 @@ function* enumerateVertices(fnArray: OPS[], argsArray: any[]): IterableIterator<
                 mat2d.multiply(currentMatrix, currentMatrix, mat2d.fromValues(a, b, c, d, tx, ty));
                 break;
             case OPS.moveTo:
+                currentPoint = vec(args[0], args[1]);
+                break;
             case OPS.lineTo:
-                for (const vec of pairwise(args))
-                    yield vec2.transformMat2d(vec2.create(), vec, currentMatrix);
+                const p = vec(args[0], args[1]);
+                if (currentPoint) {
+                    yield { start: currentPoint, end: p };
+                }
+                currentPoint = p;
                 break;
             case OPS.constructPath:
-                for (const vec of pairwise(args[1]))
-                    yield vec2.transformMat2d(vec2.create(), vec, currentMatrix);
+                const [subFn, subArgs] = args;
+                for (let j = 0; j < subFn.length; j++) {
+                    if (subFn[j] === OPS.moveTo) {
+                        currentPoint = vec(subArgs[j * 2], subArgs[j * 2 + 1]);
+                    } else if (subFn[j] === OPS.lineTo) {
+                        const p = vec(subArgs[j * 2], subArgs[j * 2 + 1]);
+                        if (currentPoint) {
+                            yield { start: currentPoint, end: p };
+                        }
+                        currentPoint = p;
+                    }
+                }
                 break;
-        }
-    }
-}
-
-function* groupByThreshold<T>(array: T[], fn: (x: T) => number, threshold: number): IterableIterator<T[]> {
-    if (array.length === 0) return;
-
-    array.sort((a, b) => fn(a) - fn(b));
-    let group: T[] = [];
-    let groupKey: number = fn(array[0]);
-    for (const x of array) {
-        const key = fn(x);
-        if (key - groupKey < threshold) {
-            group.push(x);
-        } else {
-            yield group;
-            group = [x];
-            groupKey = key;
-        }
-    }
-    yield group;
-}
-
-interface Label {
-    name: string;
-    rank: number;
-    top: number;
-    bottom: number;
-}
-
-function* enumerateLabels(vertices: vec2[], items: pdfjs.TextContentItem[]): IterableIterator<Label> {
-    // X 座標が近いものをまとめる
-    const vertexGroupingThreshold = 2;
-    const vertexGroups = groupByThreshold(vertices, v => v[0], vertexGroupingThreshold);
-
-    const textItemGroupingThreshold = 10;
-    const textItemGroups = groupByThreshold(items, t => t.transform[4], textItemGroupingThreshold);
-
-    let rank = 0;
-    const vertexGroup: vec2[] = [];
-    for (const textItemGroup of textItemGroups) {
-        rank++;
-        // 頂点追加
-        vertexGroup.push(...vertexGroups.next().value);
-        // Y 座標でソート
-        vertexGroup.sort((a, b) => a[1] - b[1]);
-
-        for (const textItem of textItemGroup) {
-            const ty: number = textItem.transform[5];
-            // Y 座標が text より高い位置にある最初の頂点
-            const topIndex = vertexGroup.findIndex(([x, y]) => y > ty);
-            yield {
-                name: textItem.str,
-                rank: rank,
-                top: vertexGroup[topIndex][1],
-                bottom: vertexGroup[topIndex - 1][1],
-            }
         }
     }
 }
